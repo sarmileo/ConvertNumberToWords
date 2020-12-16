4c087ac02b10f0739c96bb898e6a58b1c78251c7
Leonardo Sarmiento <sarmileo@yahoo.com>
2020-12-16 08:12:56 -0500

    add option menu and the option to convert number to words as currency

--- convertNumberToWords.py
+++ convertNumberToWords.py
@@ -5,13 +5,25 @@ import re
 
 number = float(input("Enter number: "))
 
-url = f'https://www.calculatorsoup.com/calculators/conversions/numberstowords.php?number={number}&letter_case=lowercase&action=solve'
+print("""
+Options
+1- Word Representation
+2- Currency
+3- Check Writing""")
+option = input("Select: ")
+
+url = ""
+if option == "1":
+    url = f'https://www.calculatorsoup.com/calculators/conversions/numberstowords.php?number={number}&letter_case=lowercase&action=solve'
+elif option == "2":
+    url = f'https://www.calculatorsoup.com/calculators/conversions/numberstowords.php?number={number}&format=currency&letter_case=lowercase&action=solve'
 resp = req.get(url)
 
 pattern = "<div id=\"answer\" class=\"still\"><br>([a-z\s\-\.]+)<\/div>"
 
 #print(resp.text) # Printing response
 numberInWords = re.search(pattern, resp.text)
+print()
 print(numberInWords[1])
 
 
